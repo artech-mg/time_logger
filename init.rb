ab634cefc0849378c5d31365243a538d6214f8e3
Moritz Gschwandtner <gschwandtner@artech.at>
2024-06-18 14:29:10 +0200

    redmine 5: modify init.rb

--- init.rb
+++ init.rb
@@ -1,20 +1,19 @@
 require 'redmine'
 
-require_dependency 'time_logger_hooks'
-
+require File.dirname(__FILE__) + '/lib/time_logger_hooks'
 # workaround helping rails to find the helper-methods
-require File.join(File.dirname(__FILE__), 'app/helpers/application_helper.rb')
+require File.dirname(__FILE__) + '/app/helpers/application_helper'
 
 Redmine::Plugin.register :time_logger do
   name 'Time Logger'
   author 'Jim McAleer'
   description 'The orignal author was Jérémie Delaitre.'
   url 'https://github.com/speedy32129/time_logger'
-  version '0.5.4'
+  version '0.6.0'
 
   # fix for contect menus
 
-  requires_redmine version_or_higher: '1.1.0'
+  requires_redmine :version_or_higher => '3.0.0'
 
   settings default: { refresh_rate: '60', status_transitions: {}, redirect_to_new_time_entry: 'off' }, partial: 'settings/time_logger'
 
